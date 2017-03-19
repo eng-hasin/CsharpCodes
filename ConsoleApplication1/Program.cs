e06f0c5027598ba09fb3540254f57233dcc08d1d
HASIN <HASIN@DESKTOP-EU95IPC>
2017-03-19 15:52:54 +0300

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime yesterday = DateTime.Now.AddDays(-1);
+            DateTime today = DateTime.Today;
+            DateTime tomorrow = DateTime.Now.AddDays(1);
+
+
+            string today1 = today.ToString("dddd");
+            string dtomorrow1 = tomorrow.ToString("dddd");
+            string yesterday1 = yesterday.ToString("dddd");
+
+            
+            Console.WriteLine("*********************");
+            Console.WriteLine(yesterday1 + "\n");
+            Console.WriteLine(today1 + "\n");
+            Console.WriteLine(dtomorrow1 + "\n");
+            Console.ReadKey();
+        }
+    }
+}
